37de2dcc0fd41396295bbfe2add5a76a9f95c7ec
Pierre Besson <pierre.besson7@gmail.com>
2016-06-28 22:02:12 +0200

    [db] init.

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const sequelizeInstance = new Sequelize('chatX', null, null, {
         dialect: 'sqlite',
         storage: join(__dirname, './db.sqlite'),
         port: 3306,
-        logging: false
+        logging: true
 });
 
 module.exports = sequelizeInstance;
