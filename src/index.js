0f5550d00344ef2b2cf297de6d4c5f24db567d2f
Pierre Besson <pierre.besson7@gmail.com>
2016-06-28 23:17:43 +0200

    [api] Add GET user/:id with db call.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 const initDB = require('./db');
-initDB();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+initDB().then(schema => {
+  app.get('/user/:id', (req, res) => {
+    schema.User.findById(req.params.id).then(user => {
+      if (!user) {
+          res.status(404);
+          res.json({error: 'No user found'});
+      } else {
+        res.json(user)
+      };
+    });
+
+  });
+  app.listen(3000, () =>  console.log('Start app'));
+});
