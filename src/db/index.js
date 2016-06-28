fafd75aba10c08a5c21919ab02cb0c02ce032ae1
Pierre Besson <pierre.besson7@gmail.com>
2016-06-28 23:31:29 +0200

    [doc] Add comments.

--- src/db/index.js
+++ src/db/index.js
@@ -6,8 +6,13 @@ module.exports = function initDB(){
   let schema;
   return db.authenticate()
     .then(() => createSchema(db))
+    // We save the schema inside a variable to return it
     .then(sch => (schema = sch))
+    // Populate the db (usefull here)
+    // This could be in a separate npm task...
     .then(() => populate(schema))
     .catch(err => console.log('Error on starting and populating DB', err))
+    // The promise always return the schema
+    // To be able ton work with it
     .then(() => schema)
 }
